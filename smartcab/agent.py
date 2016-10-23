e3c1e86f94eaeb720b399245b97a65764ed46604
Juan Carlos Sastre <juan.carlos.sastre.iglesias@gmail.com>
2016-10-23 13:45:19 +0200

    Backup commit 20161023_1345.

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -32,7 +32,7 @@ class RandomAgent(Agent):
 class QLearningAgent(Agent):
     """An agent that learns to drive in the smartcab world."""
 
-    def __init__(self, env, epsilon_rate=0.5):
+    def __init__(self, env, epsilon_rate=0.5, alpha_rate=0.5, gamma_rate=0.5):
         # sets self.env = env, state = None, next_waypoint = None, and a default color
         super(QLearningAgent, self).__init__(env)
 
@@ -42,6 +42,9 @@ class QLearningAgent(Agent):
         self.valid_actions = Environment.valid_actions
 
         self.epsilon = epsilon_rate  # Exploration rate
+        self.alpha = alpha_rate  # Learning rate
+        self.gamma = gamma_rate # Discount factor rate
+
         self.q_matrix = {}
 
     def reset(self, destination=None):
@@ -49,35 +52,55 @@ class QLearningAgent(Agent):
 
     def get_q_value(self, state, action):
         key = (state, action)
-        return self.q_matrix.get(key, 0)
+        return self.q_matrix.get(key, 0.0)
 
-    def get_max_q(self, state):
-        q = [self.get_q_value(state, a) for a in self.valid_actions]
-        return max(q)
+    # def get_max_q(self, state):
+    #     q = [self.get_q_value(state, a) for a in self.valid_actions]
+    #     return max(q)
 
     def choose_action(self, state):
-        if random.random() < self.epsilon:
-            best = [i for i in range(len(self.valid_actions)) if q[i] == maxQ]
-            i = random.choice(best)
-        else:
-            i = q.index(maxQ)
-
-        action = self.actions[i]
-        return action
-
-        if random.random() < self.epsilon:
-            action = random.choice(Environment.valid_actions)
-        else:
-            q = [self.get_q_value(state, a) for a in Environment.valid_actions]
-            if q.count(max(q)) > 1:
-                best_actions = [i for i in range(len(Environment.valid_actions)) if q[i] == max(q)]
-                index = random.choice(best_actions)
-
+        if random.random() < self.epsilon:  # explore
+            action = random.choice(self.valid_actions)
+        else:  # exploit
+            q = [self.get_q_value(state, a) for a in self.valid_actions]
+            max_q = max(q)
+            count = q.count(max_q)
+            if count > 1:
+                best = [i for i in range(len(self.valid_actions)) if q[i] == max_q]
+                i = random.choice(best)
+                action = self.valid_actions[i]
             else:
-                index = q.index(max(q))
-            action = Environment.valid_actions[index]
+                i = q.index(max_q)
+                action = self.valid_actions[i]
 
         return action
+
+    def learn(self, previous_state, action, current_state, reward):
+        max_q_new = max([self.get_q_value(current_state, a) for a in self.valid_actions])
+
+        old_q_value = self.q_matrix.get((state_1, action), None)
+
+        value = reward + self.gamma * max_q_new
+
+        if old_q_value is None:
+            self.q_matrix[(state_1, action)] = reward
+        else:
+            self.q_matrix[(state_1, action)] = old_q_value + self.alpha * (value - oldv)
+
+
+    #     key = (state, action)
+    #
+    #     if (key not in self.q_matrix):
+    #
+    #     self.get_q_value(state, action)
+    #
+    #     key = (state, action)
+    #         if (key not in self.qDict):
+    #             # initialize the q values
+    #             self.qDict[key] = 5.0
+    #         else:
+    #
+    # self.qDict[key] = self.qDict[key] + self.alpha * (reward + self.gamma * self.getMaxQ(nextState) - self.qDict[key])
 
     def update(self, t):
         # Gather inputs
@@ -87,26 +110,21 @@ class QLearningAgent(Agent):
         deadline = self.env.get_deadline(self)
 
         # Update state
-        self.state = inputs
-        self.state['next_waypoint'] = self.next_waypoint
+        self.current_state = inputs
+        self.current_state['next_waypoint'] = self.next_waypoint
 
         # Select action according to your policy
-        action = self.get_action_based_on_epsilon_policy(self.state)
-
-        # action = self.state['next_waypoint']
-        # if action == 'forward':
-        #     if self.state['light'] == 'red':
-        #         action = None
-        # elif action == 'right':
-        #     if self.state['light'] == 'red' or self.state['left'] == 'forward':
-        #         action = None
-        # elif action == 'left':
-        #     if self.state['light'] == 'red' or (self.state['oncoming'] == 'forward' or self.state['oncoming'] ==
-        #         'right'):
-        #         action = None
+        action = self.choose_action(self.current_state)
 
         # Execute action and get reward
         reward = self.env.act(self, action)
+
+        # TODO: Learn policy based on state, action, reward
+        if reward is not None:
+            self.learn(self.previous_state, self.action, self.current_state, reward)
+
+        # Backup current state for next iteration
+        self.previous_state = self.current_state
 
         print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs,
                                                                                                     action, reward)  # [debug]
